12b26dda66c8b62d3c063bdbf19db7efceb8f246
amirthap03 <115933113+amirthap03@users.noreply.github.com>
2023-07-12 09:59:19 +0530

    Update Of 5 numbers, how many are even or odd.java

--- Day 2 (Wipro Talent Next Java Training)/Of 5 numbers, how many are even or odd.java
+++ Day 2 (Wipro Talent Next Java Training)/Of 5 numbers, how many are even or odd.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 class UserMainCode{
-    public static int countEvenNumbers(int input1, int input2, int int count = 0;
+    public static int countEvenNumbers(int input1, int input2, int int count = 0;{
         int count = 0;
         if (input6.equals("even")) {
             if (input1 % 2 == 0) count++;
@@ -16,7 +16,6 @@ class UserMainCode{
             if (input4 % 2 != 0) count++;
             if (input5 % 2 != 0) count++;
         }
-        
         return count;
     }
 }
